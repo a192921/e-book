5a37fd2ff7b4a9295714c8f09cc411c148ac6d5c
queena wu <ac192921@gmail.com>
2020-06-24 10:27:55 +0800

    20200623

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
-var up_lev = document.getElementById("up_lev");
+// var up_lev = document.getElementById("up_lev");
+
+var icd_input;
+
+function icd_search_Function() {
+    icd_input = document.getElementById("icd").value;
+    alert(icd_input);
+}
